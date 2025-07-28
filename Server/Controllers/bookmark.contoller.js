de31125dce8797f3a10980785e32dc177d3ef6dd
DiyanshuVortex <divyanshuchandra9027@gmail.com>
2025-07-28 15:06:41 +0530

    Bookmark-FE

--- Server/Controllers/bookmark.contoller.js
+++ Server/Controllers/bookmark.contoller.js
@@ -1,10 +1,35 @@
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
 
 export const bookmarks = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select("booksId");
     res.status(200).json({ bookmarks: user.booksId });
   } catch (err) {
-    res.status(500).json({ message: "Error fetching bookmarks", error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error fetching bookmarks", error: err.message });
   }
 };
+
+export const bookmarksadd = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const bookIdToAdd = req.body.bookId;
+    if (!bookIdToAdd) {
+      return res.status(400).json({ message: "bookId is required" });
+    }
+
+    // Avoid duplicates
+    if (!user.booksId.includes(bookIdToAdd)) {
+      user.booksId.push(bookIdToAdd);
+      await user.save();
+    }
+
+    res.status(200).json({ message: "Book added to bookmarks", booksId: user.booksId });
+  } catch (error) {
+    console.log("bookmark controller error:", error);
+    res.status(500).json({ message: "Error adding bookmark", error: error.message });
+  }
+};
+
