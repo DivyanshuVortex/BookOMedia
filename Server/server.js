82b252bd530e89af0c5dc52b4db080637eaa14e6
DiyanshuVortex <divyanshuchandra9027@gmail.com>
2025-08-20 01:47:33 +0530

    basic fix

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://your-frontend.vercel.app"
+    "https://book-o-media.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
