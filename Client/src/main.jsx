19ac271c1ad6553047d4d004c5954f5b29364f42
DiyanshuVortex <divyanshuchandra9027@gmail.com>
2025-08-20 01:42:39 +0530

    FIXS:: DEPLOYEMNT ISSUES

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,4 +1,6 @@
 import { StrictMode } from 'react'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,5 +8,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
